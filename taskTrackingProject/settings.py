35fa8ea531b044ac85d88fc52ef83fd42dd6c7da
devPrasanthKumar <prok52010get@gmail.com>
2024-01-09 22:05:33 -0800

    m

--- taskTrackingProject/settings.py
+++ taskTrackingProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "ccfd09ade43353745e8a5a76f8087711"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
